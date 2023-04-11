d9831b73dffeeed809f6e509e2e7ef1f098ab084
SanGiRahadian <giegi1206@gmail.com>
2023-04-11 15:59:32 +0700

    uts

--- resources/views/positions/create.blade.php
+++ resources/views/positions/create.blade.php
@@ -1,27 +1,27 @@
 @extends('app')
 @section('content')
-<form action="{{ route('departements.store') }}" method="POST" enctype="multipart/form-data">
-            @csrf
-            <div class="row">
-                <div class="col-xs-12 col-sm-12 col-md-12">
-                    <div class="form-group">
-                        <strong>Departement Name:</strong>
-                        <input type="text" name="name" class="form-control" placeholder="Departement Name">
-                        @error('name')
-                        <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
-                        @enderror
-                    </div>
-                </div>
-                <div class="col-xs-12 col-sm-12 col-md-12">
-                    <div class="form-group">
-                        <strong>Keterangan:</strong>
-                        <input type="keterangan" name="keterangan" class="form-control" placeholder="Keterangan">
-                        @error('keterangan')
-                        <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
-                        @enderror
-                    </div>
-                </div>
-                <div class="col-xs-12 col-sm-12 col-md-12">
+  <form action="{{ route('positions.store') }}" method="POST" enctype="multipart/form-data">
+      @csrf
+      <div class="row">
+          <div class="col-xs-12 col-sm-12 col-md-12">
+              <div class="form-group">
+                  <strong>Name:</strong>
+                  <input type="text" name="name" class="form-control" placeholder="Nama Lengkap">
+                  @error('name')
+                  <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
+                  @enderror
+              </div>
+          </div>
+          <div class="col-xs-12 col-sm-12 col-md-12">
+              <div class="form-group">
+                  <strong>Keterangan:</strong>
+                  <input type="text" name="keterangan" class="form-control" placeholder="Keterangan Posisi">
+                  @error('keterangan')
+                  <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
+                  @enderror
+              </div>
+          </div>
+          <div class="col-xs-12 col-sm-12 col-md-12">
                     <div class="form-group">
                         <strong>Singkatan:</strong>
                         <input type="alias" name="alias" class="form-control" placeholder="Singkatan">
@@ -29,8 +29,8 @@
                         <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
                         @enderror
                     </div>
-                </div>
-                <button type="submit" class="btn btn-primary ml-3">Submit</button>
             </div>
-        </form>
+          <button type="submit" class="btn btn-primary mt-3 ml-3">Submit</button>
+      </div>
+  </form>
 @endsection
